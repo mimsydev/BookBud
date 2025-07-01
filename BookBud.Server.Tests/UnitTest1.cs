2736c9b03dc1616d34de4add04285972bee76b4a
Johnathan Rhoades <jrhoades7989@gmail.com>
2025-07-01 08:37:52 -0400

    Adding unit tests

---
+++ BookBud.Server.Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookBud.Server.Services;
+using BookBud.Server.Models;
+
+namespace BookBud.Server.Tests
+{
+    public class TestDatabaseFixture : DbContext
+    {
+        private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=BookBudTest;Trusted_Connection=True;ConnectRetryCount=0;Encrypt=False";
+        private static readonly Lock _lock = new();
+        private static bool _databaseInitialized;
+
+        public TestDatabaseFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext()) {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+                        context.AddRange(
+                            new BookDetail
+                            {
+                                Id = new Guid("5C60F693-BEF5-E011-A485-80EE7300C692"),
+                                Title = "Atlas Shrugged",
+                                Author = "Ayn Rand",
+                                CreateDate = Convert.ToDateTime("07/01/2025 11:59"),
+                                ISBN10 = "1234567890",
+                                ImageUrl = "https://www.google.com"
+                            }); 
+                    }
+                    _databaseInitialized = true;
+                }
+            }
+        }
+        public static BookBudContext CreateContext() => new (
+            new DbContextOptionsBuilder<BookBudContext>()
+            .UseSqlServer(ConnectionString)
+            .Options);
+    }
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- BookBud.Server/Models/BookDetail.cs
+++ BookBud.Server/Models/BookDetail.cs
@@ -3,9 +3,9 @@ namespace BookBud.Server.Models
     public class BookDetail
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string ISBN10 { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public required string ISBN10 { get; set; }
         public string? ImageUrl { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime UpdateDate { get; set; }
