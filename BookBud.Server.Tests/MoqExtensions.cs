f5cc7af59b857b190714640eacb5e7453039f368
Johnathan Rhoades <jrhoades7989@gmail.com>
2025-07-04 11:19:20 -0400

    Middle of getting Moq extension working

---
+++ BookBud.Server.Tests/MoqExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Internals;
+using Moq.Protected;
+
+namespace BookBud.Server.Tests
+{
+    public static class MoqExtensions
+    {
+        public static ISetup<HttpMessageHandler, Task<HttpResponseMessage>> SetupSendAsync<Task<HttpResponseMessage>>(
+            this Mock<HttpMessageHandler> handler, HttpMethod requestMethod, string requestUrl)
+        {
+            return handler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(r =>
+                    r.Method == requestMethod &&
+                    r.RequestUri != null &&
+                    r.RequestUri.ToString() == requestUrl
+                    ),
+                    ItExpr.IsAny<CancellationToken>()
+                );
+        }
+    }
+}
--- BookBud.Server.Tests/OpenLibraryTest.cs
+++ BookBud.Server.Tests/OpenLibraryTest.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using BookBud.Server.Models;
+using BookBud.Server.Services;
+using BookBud.Server.Services.OpenLib;
+using Moq.Protected;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace BookBud.Server.Tests
 {
-    internal class OpenLibraryTest
+    public class OpenLibraryTest
     {
+        private readonly OpenLibService _openLibraryService;
+        private readonly HttpClient _httpClient;
+        private static readonly string[] _jsonGetBooksContent = [" "];
+
+        public OpenLibraryTest()
+        {
+            var handlerMock = new Mock<HttpMessageHandler>();
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "GetBooksAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken> ())
+                .ReturnsAsync(new HttpResponseMessage() 
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = JsonContent.Create(_jsonGetBooksContent)
+                });
+
+            _openLibraryService = new OpenLibService()
+            _httpClient = httpClient;
+        }
+
+        [Fact]
+        public async Task GetBooksAsync_ReturnsBooks()
+        {
+            
+        }
     }
 }
